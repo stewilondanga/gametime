7c8ef511303827c9714cddc635829e25cbe0e2f4
Stewart ilondanga <stilondanga@gmail.com>
2017-12-06 11:20:30 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
-(document).ready(function() {
-  ("form#game-time").submit(function(event) {
-   event.preventDefault();
-   var number = parseInt($("input#number").val());
-   var result = leapYear(year);
-    ("#result").text(result);
+//#logic inteface
+
+var theFormNumbers =[];
+function stewart(finisher) {
+  for (var index = 1; index <=finisher; index++)
+        if (index %15===0) {
+    theFormNumbers.push("ping pong");
+    }else if (index % 5===0) {
+      theFormNumbers.push("pong");
+    } else if (index %3===0) {
+      theFormNumbers.push("ping");
+    }else {
+      theFormNumbers.push(index);
+    }
+    }
+// #user interface
+
+$(document).ready(function() {
+  $("#button").click(function() {
+    var finisher = parseInt($("#numberInput").val());
+    stewart(finisher);
+    theFormNumbers.forEach(function(finisher){
+      $("#results").append("<li>" +finisher + "</li>");
+    });
+
   });
 });
